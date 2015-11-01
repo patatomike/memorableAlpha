d7b118785056b995cd562c98d2f48ab1490f519b
Michaël Dayen <dayen.michael@gmail.com>
2015-10-31 22:34:50 -0400

    code import #1
    michael

---
+++ js/app.js
@@ -0,0 +1,16 @@
+angular.module('App', ['ngRoute', 'App.services', 'App.controllers', 'google-maps'])
+    // .config(['$compileProvider', function ($compileProvider) {
+    //     $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    // }])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller: 'PointOfInterestsController',
+            templateUrl: 'partials/memorableNOW.html'
+        })
+        .when('/live', {
+            controller: 'PointOfInterestsController',
+            templateUrl: 'partials/memorableLIVE.html'
+        })
+        .otherwise({redirectTo: '/'});
+    });
---
+++ js/services.js
@@ -0,0 +1,82 @@
+angular.module('App.services', [])
+    .factory('cordovaReady', [function () {
+        return function (fn) {
+            var queue = [],
+                impl = function () {
+                    queue.push([].slice.call(arguments));
+                };
+
+            document.addEventListener('deviceready', function () {
+                queue.forEach(function (args) {
+                    fn.apply(this, args);
+                });
+                impl = fn;
+            }, false);
+
+            return function () {
+                return impl.apply(this, arguments);
+            };
+        };
+    }])
+    .factory('MarkerCreatorService', function () {
+
+    var markerId = 0;
+
+    function create(latitude, longitude) {
+        var marker = {
+            options: {
+                animation: 1,
+                labelAnchor: "28 -5",
+                labelClass: 'markerlabel'
+            },
+            latitude: latitude,
+            longitude: longitude,
+            id: ++markerId
+        };
+        return marker;
+    }
+
+    function invokeSuccessCallback(successCallback, marker) {
+        if (typeof successCallback === 'function') {
+            successCallback(marker);
+        }
+    }
+
+    function createByCoords(latitude, longitude, successCallback) {
+        var marker = create(latitude, longitude);
+        invokeSuccessCallback(successCallback, marker);
+    }
+
+    function createByAddress(address, successCallback) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address' : address}, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                var firstAddress = results[0];
+                var latitude = firstAddress.geometry.location.lat();
+                var longitude = firstAddress.geometry.location.lng();
+                var marker = create(latitude, longitude);
+                invokeSuccessCallback(successCallback, marker);
+            } else {
+                alert("Unknown address: " + address);
+            }
+        });
+    }
+
+    function createByCurrentLocation(successCallback) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var marker = create(position.coords.latitude, position.coords.longitude);
+                invokeSuccessCallback(successCallback, marker);
+            });
+        } else {
+            alert('Unable to locate current position');
+        }
+    }
+
+    return {
+        createByCoords: createByCoords,
+        createByAddress: createByAddress,
+        createByCurrentLocation: createByCurrentLocation
+    };
+
+});
