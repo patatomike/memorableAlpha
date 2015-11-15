c626618930ed2034c4fb8c8332a9e382b4c04688
Michaël Dayen <dayen.michael@gmail.com>
2015-11-15 05:38:13 -0500

    www

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ angular.module('App.controllers', [])
     .controller('PointOfInterestsController', ['$scope', function ($scope) {
         $scope.status = "It works!";
         $scope.pointOfInterests = [
-              { img: '../img/sg-005.jpg', distance: '200m', direction: 'NW' },
+              { img: 'img/sg-005.jpg', distance: '200m', direction: 'NW' },
               { img: 'http://www.roomsbooking.com/wp-content/uploads/2014/11/Merloin-Park-Singapore.jpg', distance: '600m', direction: 'NE' },
               { img: 'http://www.travelandleisure.com/sites/default/files/styles/weekend_getaways_landing_page/public/singapore19650815-005.jpg?itok=VeA42doQ', distance: '100m', direction: 'SW' },
               { img: 'http://www.roomsbooking.com/wp-content/uploads/2014/11/Singapore-Night-Markets.jpg', distance: '500m', direction: 'SE' }
