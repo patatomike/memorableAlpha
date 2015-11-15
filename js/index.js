736866e7a618c7141e5fc6230fd5455d5f1ca4cf
raphael <raphael.sculati@gmail.com>
2015-11-15 23:23:20 +0800

    Marker position

--- js/index.js
+++ js/index.js
@@ -51,32 +51,23 @@ var app = {
         var transitLayer = new google.maps.TransitLayer();
         transitLayer.setMap(map);
 
-        // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+        var myloc = new google.maps.Marker({
+          clickable: false,
+          icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+                new google.maps.Size(22,22),
+                new google.maps.Point(0,18),
+                new google.maps.Point(11,11)),
+                shadow: null,
+                zIndex: 999,
+                map: // your google.maps.Map object
+        });
 
-      var infoWindow = new google.maps.InfoWindow({map: map});
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Hello you.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
+        if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function(pos) {
+          var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+          myloc.setPosition(me);
+        }, function(error) {
+          // ...
+        });
 
 
         map.initialize();
