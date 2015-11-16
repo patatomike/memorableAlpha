95221f7ab1e94c414cb9d94f869c75a47a8d0339
raphael <raphael.sculati@gmail.com>
2015-11-16 16:34:31 +0800

    https not defined modif

--- js/services/serviceMap.js
+++ js/services/serviceMap.js
@@ -2,7 +2,7 @@ angular.module('App')
   .factory('serviceMap', function serviceMap($http) {
     return{
           getPlaces: function(page){
-              return $https.get("https://memorablebackend.herokuapp.com/api/places/1.300529/103.861990/2000");
+              return $http.get("https://memorablebackend.herokuapp.com/api/places/1.300529/103.861990/2000");
           }
       }
   });
