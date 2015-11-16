708cc9587dcdfffdf9c8aacf276cd3c9768888cb
raphael <raphael.sculati@gmail.com>
2015-11-16 17:21:09 +0800

    images news

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,13 @@ angular.module('App.controllers', [])
     .controller('PointOfInterestsController', function ($scope, serviceMap ,$http) {
         $scope.status = "It works!";
 
+        $scope.pointOfInterests = [
+              { img: 'https://s3-us-west-1.amazonaws.com/memorable/buddha_tooth_relic_temple_and_museum.jpg', distance: '200m', direction: 'NW' },
+              { img: 'https://s3-us-west-1.amazonaws.com/memorable/merlion_park.jpg', distance: '600m', direction: 'NE' },
+              { img: 'https://s3-us-west-1.amazonaws.com/memorable/chinatown_singapore.jpg', distance: '100m', direction: 'SW' },
+              { img: 'https://s3-us-west-1.amazonaws.com/memorable/marina_bay_sands.jpg', distance: '500m', direction: 'SE' }
+            ];
+
         // call factory to retrieve place nearby
         serviceMap.getPlaces(1.300529,103.861990,2000).success(function(data){
           console.log(data);
@@ -54,19 +61,14 @@ angular.module('App.controllers', [])
 
           navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-          map.initialize();
+          // map.initialize();
 
 
 
         // console.log($http.get('https://memorablebackend.herokuapp.com/api/places/1.300529/103.861990/2000'));
 
 
-        $scope.pointOfInterests = [
-              { img: 'https://s3-us-west-1.amazonaws.com/memorable/buddha_tooth_relic_temple_and_museum.jpg', distance: '200m', direction: 'NW' },
-              { img: 'https://s3-us-west-1.amazonaws.com/memorable/merlion_park.jpg', distance: '600m', direction: 'NE' },
-              { img: 'https://s3-us-west-1.amazonaws.com/memorable/chinatown_singapore.jpg', distance: '100m', direction: 'SW' },
-              { img: 'https://s3-us-west-1.amazonaws.com/memorable/marina_bay_sands.jpg', distance: '500m', direction: 'SE' }
-            ];
+
 
 
     })
