88ef93ab5e1114bc9cfa2d326f2249e084ea11f2
raphael <raphael.sculati@gmail.com>
2015-11-16 17:14:01 +0800

    Intregration Map Controllre

--- js/index.js
+++ js/index.js
@@ -43,52 +43,6 @@ var app = {
     receivedEvent: function(id) {
 
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: {lat: 1.290299, lng: 103.850143},
-            streetViewControl: false,
-            disableDefaultUI: true
-          });
-
-        var transitLayer = new google.maps.TransitLayer();
-        transitLayer.setMap(map);
-
-        var onSuccess = function(position) {
-        console.log('Latitude: '          + position.coords.latitude          + '\n' +
-            'Longitude: '         + position.coords.longitude         + '\n' +
-            'Altitude: '          + position.coords.altitude          + '\n' +
-            'Accuracy: '          + position.coords.accuracy          + '\n' +
-            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-            'Heading: '           + position.coords.heading           + '\n' +
-            'Speed: '             + position.coords.speed             + '\n' +
-            'Timestamp: '         + position.timestamp                + '\n');
-
-            var myLatlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
-            var image = 'img/TrackingDot.png';
-            var marker = new google.maps.Marker({
-              position: myLatlng,
-              icon: image,
-              title:"Hello you!"
-            });
-
-            marker.setMap(map);
-          };
-
-
-          function onError(error) {
-            console.log('code: '    + error.code    + '\n' +
-            'message: ' + error.message + '\n');
-          }
-
-          navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-
-
-
-        map.initialize();
-
-
         console.log('Received Event: ' + id);
     }
 };
